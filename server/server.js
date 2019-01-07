da90ff41384c84d3fbdc316b21536d4ea5ec2ba9
William Pereira <william.pereira@me.com>
2019-01-07 00:13:25 -0200

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app)
+var io = socketIO(server)
+
 var port = process.env.PORT || 3000;
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+
+
+    socket.on('disconnect',()=>{
+        console.log('Disconnected from server');
+        
+    })
+    
+})
+
+server.listen(port, () => {
 	console.log('Server is UP');
 });
 
